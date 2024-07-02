421bdfc19ca453ec95659dc8f8f70bb4468516d8
Dimash <dimashaijarikov@gmail.com>
2024-07-03 00:53:05 +0500

    Initial Commit

---
+++ learning-project/learning_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_projectApp.swift
+//  learning-project
+//
+//  Created by Динмуханбет Айжарыков on 03.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct learning_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
